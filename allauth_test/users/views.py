d3b3ff2b8d9db839ddb0154082338ab68dec6913
vaanskii <giorgivanadze03@gmail.com>
2024-04-20 14:53:07 +0400

    first commit

---
+++ allauth_test/users/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import logout
+
+def home(request):
+    return render(request, 'home.html')
+
+def logout_view(request):
+    logout(request)
+    return redirect('accounts/login/')
+
+def profile(request):
+    return render(request, 'profile.html')
